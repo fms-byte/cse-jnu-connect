be22e4c517d9955d58dcaff6396b2dedd34b99c0
AhmedTrooper <AhmedTrooper@users.noreply.github.com>
2025-08-02 02:59:22 +0600

    Test commit 0001

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
@@ -5,6 +6,7 @@ const Home = () => {
     <div className='flex flex-col items-center justify-center h-screen'>
       <h1 className='text-4xl font-bold'>Welcome to CSE-JNU Connect</h1>
       <p className='text-lg mt-4'>A community platform for CSE-JNU students!</p>
+      <Button color='#abdfed'>Click me to learn more....</Button>
     </div>
   )
 }
